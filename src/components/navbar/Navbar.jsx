af61f58b57d4b4b3fc294341680a3973b09f69c6
Kashiff Khan <kashiff.k@yahoo.com>
2020-12-08 17:28:34 -0500

    menu bar started

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import { useState } from 'react';
 import './Navbar.css'
 
 export default function Navbar() {
+
+  const [bars, setBars] = useState(false)
+
+  function onMenuClick() {
+    setBars(!bars)
+  }
+
   return (
-    <div>
+    <div className="menu_bar" onClick={onMenuClick}>
+      <div className={bars === false ? "menu" : "menu_change"} >
+        <div id="bar1" className="bar"></div>
+        <div id="bar2" className="bar"></div>
+        <div id="bar3" className="bar"></div>
+      </div>
     </div>
   );
 }
