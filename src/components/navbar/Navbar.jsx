bbffe250fd9081c9d2fc3b1016aa904b0fcff5e6
Kashiff Khan <kashiff.k@yahoo.com>
2020-12-08 20:52:15 -0500

    NavBar complete

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,11 +10,20 @@ export default function Navbar() {
   }
 
   return (
-    <div className="menu_bar" onClick={onMenuClick}>
-      <div className={bars === false ? "menu" : "menu_change"} >
+    <div className={bars === false ? "menu_bar" : "menu_bar_change"}>
+      <p id="home_page_menu_item" className={bars===false ? "menu_item" : "menu_item_change"}>Home Page</p>
+      <p id="about_me_menu_item" className={bars===false ? "menu_item" : "menu_item_change"}>About Me</p>
+      <p id="my_projects_menu_item" className={bars===false ? "menu_item" : "menu_item_change"}>My Projects</p>
+      <p id="my_projects_menu_item" className={bars===false ? "menu_item" : "menu_item_change"}>A Junior Dev's Blog</p>
+      <p id="contact_me_menu_item" className={bars===false ? "menu_item" : "menu_item_change"}>Contact Me</p>
+      
+      <div className={bars === false ? "menu" : "menu_change"} onClick={onMenuClick}>
         <div id="bar1" className="bar"></div>
         <div id="bar2" className="bar"></div>
         <div id="bar3" className="bar"></div>
+      </div>
+      <div className="navbar_choices">
+
       </div>
     </div>
   );
